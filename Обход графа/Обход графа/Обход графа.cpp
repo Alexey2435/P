25a0ec3a5614d8eeb1ef4648355ced910982bc50
Alexey2435 <78438883+Alexey2435@users.noreply.github.com>
2022-11-21 13:02:55 +0300

    Add files via upload

---
+++ Обход графа/Обход графа/Обход графа.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int N = 4;
+int Visited[N] = { 0, 0, 0, 0};
+
+void DFS(int s, int** M) {
+    cout << s << endl;
+    Visited[s - 1] = 1;
+    for (int i = 1; i <= N; i++)
+        if (M[s - 1][i - 1] == 1 && Visited[i-1] == 0)
+        {
+            //Visited[i - 1] = 1;
+            DFS(i, M);
+        }
+}
+
+int main()
+{
+    srand(time(NULL));
+    int s = 1;
+    int** M = (int**)malloc(N * sizeof(int));
+    for (int i = 0; i < N; i++)
+    {
+        M[i] = (int*)malloc(N * sizeof(int));
+    }
+    for (int i = 0; i < N; i++)                     //Заполнение
+    {
+        for (int j = 0; j < N; j++)
+        {
+            M[i][j] = rand() % 2;
+            M[j][i] = M[i][j];
+            if (i == j)
+                M[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << M[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+
+    DFS(s, M);
+}
